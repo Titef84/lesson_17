43a45527d8c43971ba6ab48c7ac1232de45b84d0
Oleksandr Prokhodets <titef84@gmail.com>
2021-12-04 15:13:23 +0200

    Начал делать 7 задание

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,4 +44,57 @@ console.log(counting.value(200));
 counting.increment();
 console.log(counting.value());
 
+let myPrint = (a, b, res) => `${a}^${b}=${res}`;
+function myPow (a, b, callback) {
+  function pow (x, n) {
+    if (n !== 1) return x *= pow(x,n - 1);
+    return x; 
+  };
+  return callback(a, b, pow(a, b,));
+};
+console.log(myPow(3, 4, myPrint));
+console.log(myPow(2, 3, myPrint));
 
+function info () {
+  return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+};
+let yearNow = new Date().getFullYear();
+let car = {
+  engine: 2000,
+  model: 'Lacetti',
+  name: 'Chevrolet',
+  year: 2010,
+  info: info,
+  get used () {
+    return this.year !== yearNow ? 'used' : 'new';
+  },
+
+set used(value) {
+  if (value === 'new' && this.year < yearNow) this.year = yearNow;
+}
+};
+
+let car2 = {
+  engine: 5000,
+  model: 'FX50 AWD',
+  name: 'Infinite',
+  year: 2019,
+  info: info,
+  get used () {
+    return this.year !== yearNow ? 'used' : 'new';
+  },
+
+set used(value) {
+  if (value === 'new' && this.year < yearNow) this.year = yearNow;
+}
+};
+console.log(car.info());
+car.used = 'new';
+console.log(car.info());
+car.used = 'used';
+console.log(car.info());
+console.log(car2.info());
+car.used = 'used';
+console.log(car2.info());
+
+let list
